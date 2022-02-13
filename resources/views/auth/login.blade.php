9517d77c6ef6305270a53f344779130703e7f2c2
miyazaki0505 <mr.gingerman0505@gmail.com>
2022-02-13 10:15:15 +0000

    ログイン機能作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,12 @@
 @section('content')
 
 <div class="text-center">
-    <h3 class="register">Sign Up</h3>
+    <h3 class="register">Login</h3>
 </div>
 
 <div class="row mt-5 mb-5">
     <div class="col-sm-6 offset-sm-3">
-        {!! Form::open(['route' => 'signup.post']) !!}
-            <div class="form-group">
-                {!! Form::label('name', '名前') !!}
-                {!! Form::text('name', old('name'), ['class' => 'form-control']) !!}
-            </div>
-
+        {!! Form::open(['route' => 'login.post']) !!}
             <div class="form-group">
                 {!! Form::label('email', 'メールアドレス') !!}
                 {!! Form::text('email', old('email'), ['class' => 'form-control']) !!}
@@ -24,15 +19,13 @@
                 {!! Form::password('password', ['class' => 'form-control']) !!}
             </div>
 
-            <div class="form-group">
-                {!! Form::label('password_confirmation', 'パスワード確認') !!}
-                {!! Form::password('password_confirmation', ['class' => 'form-control']) !!}
-            </div>
-
             <div class="text-center mt-5">
-                {!! Form::submit('Sign Up', ['class' => 'btn btn-secondary mt-2']) !!}
+                {!! Form::submit('Login', ['class' => 'btn btn-secondary mt-2']) !!}
             </div>
         {!! Form::close() !!}
+
+        <p class="text-center mt-5">{!! link_to_route('signup', 'Sign Up') !!}</p>
+
     </div>
 </div>
 
